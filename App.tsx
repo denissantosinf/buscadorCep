9aa48da668e006c03b0214d0f4391b0412ebb58a
denissantosinf <denis.santos.dev@gmail.com>
2024-02-24 22:45:12 -0300

    Primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,130 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from "react"
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Alert } from "react-native"
+import Api from './src/services/api'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const [cep, setCep] = useState("")
+  const [logradouro, setLogradouro] = useState("")
+  const [bairro, setBairro] = useState("")
+  const [localidade, setLocalidade] = useState("")
+  const [uf, setUf] = useState("")
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  async function buscarCep(){
+    if(cep == ""){
+      Alert.alert("Cep Inválido!")
+      setCep("")  
+    }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    try{
+        const response = await Api.get('/${cep}/json/')
+    }catch(error){
+      console.log("ERRO" + error)
+    }
+
+
+  }
+
+  return(
+    <View style={styles.containerPrincipal}>
+      
+      <View style={styles.topBar}>
+        <Text style={styles.title}>Buscador de Cep</Text>
+      </View>
+
+      <View style={styles.containerCep}>
+
+        <TextInput
+        style={{
+          borderColor: "#000000", borderWidth: 2, width: 200, fontSize: 18, 
+          marginTop: 30, marginEnd: 20, borderRadius: 10, padding: 15
+         }}
+        value={cep}
+        onChangeText={(texto) => setCep(texto)}
+        placeholder="Cep"/>
+
+      <TouchableOpacity style={styles.botaoBuscar} onPress={buscarCep}>
+        <Text style={styles.textoBotaoBuscar}>Buscar</Text>
+      </TouchableOpacity>
+
+      </View>
+
+      <TextInput
+        style={styles.caixaTexto}
+        value={logradouro}
+        onChangeText={(texto) => setLogradouro(texto)}
+        placeholder="Logradouro"/>
+
+      <TextInput
+        style={styles.caixaTexto}
+        value={bairro}
+        onChangeText={(texto) => setBairro(texto)}
+        placeholder="Bairro"/>
+
+      <TextInput
+        style={styles.caixaTexto}
+        value={localidade}
+        onChangeText={(texto) => setLocalidade(texto)}
+        placeholder="Cidade"/>
+
+      <TextInput
+        style={{
+          borderColor: "#000000", borderWidth: 2, width: 100, fontSize: 18, 
+          marginTop: 10, marginEnd: 20, borderRadius: 10, marginHorizontal: 20, padding: 15
+         }}
+        value={uf}
+        onChangeText={(texto) => setUf(texto)}
+        placeholder="Estado"/>
+     
+
     </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  containerPrincipal: {
+    flex: 1,
+    flexDirection: "column"
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  topBar: {
+    flexDirection: "row",
+    height: 70,
+    backgroundColor: "#018786"
   },
-  sectionDescription: {
-    marginTop: 8,
+  title: {
+    color: "#ffffff",
+    fontSize: 25,
+    fontWeight: "bold",
+    alignSelf: "center",
+    margin: 20,
+  },
+  containerCep: {
+    flexDirection: "row",
+    height: 100,
+    marginHorizontal: 20,
+  },
+  botaoBuscar: {
+    backgroundColor: "#018786",
+    width: 120,
+    height: 70,
+    marginTop: 30,
+    marginEnd: 20,
+    borderRadius: 10,
+    padding: 20,
+  },
+  textoBotaoBuscar: {
+    color: "#ffffff",
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "bold",
+    alignSelf: "center"
   },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  caixaTexto: {
+    borderColor: "#000000",
+    borderWidth: 2,
+    padding: 15,
+    fontSize: 18,
+    borderRadius: 10,
+    marginTop: 10,
+    marginHorizontal: 20
+  }
+})
